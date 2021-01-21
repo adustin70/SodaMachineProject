0a3950fe8a21c4e72593e0e85069d7de756c9e21
Dustin <adustin70@gmail.com>
2021-01-21 12:46:22 -0500

    Added functionality to all methods in customer class

--- SodaMachine/Customer.cs
+++ SodaMachine/Customer.cs
@@ -20,29 +20,50 @@ namespace SodaMachine
         }
         //Member Methods (Can Do)
 
-        //This method will be the main logic for a customer to retrieve coins form their wallet.
+        //This method will be the main logic for a customer to retrieve coins from their wallet.
         //Takes in the selected can for price reference
         //Will need to get user input for coins they would like to add.
-        //When all is said and done this method will return a list of coin objects that the customer will use a payment for their soda.
+        //When all is said and done this method will return a list of coin objects that the customer will use as payment for their soda.
         public List<Coin> GatherCoinsFromWallet(Can selectedCan)
         {
-          
+            List<Coin> userPayment = new List<Coin>();
+            
+            string resultOfCoinSelection = UserInterface.CoinSelection(selectedCan, Wallet.Coins);
+
+            while (Wallet.Coins.Contains(GetCoinFromWallet(resultOfCoinSelection)))
+            {
+                Wallet.Coins.Remove(GetCoinFromWallet(resultOfCoinSelection));
+                userPayment.Add(GetCoinFromWallet(resultOfCoinSelection));
+            }
+            return userPayment;
+
+            //need a way to get user input to add coins
+            //needs to return list of coins to be used for payment
+            //What can I use to capture coins selected by user then return that as a list of coins to be used for payment
+            
         }
         //Returns a coin object from the wallet based on the name passed into it.
         //Returns null if no coin can be found
         public Coin GetCoinFromWallet(string coinName)
         {
-            
+            foreach (Coin coin in Wallet.Coins)
+            {
+                if (coin.Name == coinName)
+                {
+                    return coin;
+                }
+            }
+            return null;
         }
         //Takes in a list of coin objects to add into the customers wallet.
         public void AddCoinsIntoWallet(List<Coin> coinsToAdd)
         {
-            
+            Wallet.Coins.AddRange(coinsToAdd);
         }
         //Takes in a can object to add to the customers backpack.
         public void AddCanToBackpack(Can purchasedCan)
         {
-            
+            Backpack.cans.Add(purchasedCan);
         }
     }
 }
